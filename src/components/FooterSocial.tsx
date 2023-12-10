b9b1981f82b845e070545a2749e0cc43cd475a56
kylejoe <halterman.kyle@gmail.com>
2023-12-09 22:02:36 -0800

    Updates footer

--- src/components/FooterSocial.tsx
+++ src/components/FooterSocial.tsx
@@ -1,36 +1,54 @@
 "use client";
-import { Container, Group, ActionIcon, rem, Image, Box } from "@mantine/core";
+import {
+  Container,
+  Group,
+  ActionIcon,
+  rem,
+  Image,
+  Box,
+  Title,
+  Text,
+} from "@mantine/core";
 import {
   IconBrandTwitter,
   IconBrandYoutube,
   IconBrandInstagram,
 } from "@tabler/icons-react";
-import { MantineLogo } from "@mantine/ds";
 import classes from "./FooterSocial.module.css";
 import Link from "next/link";
 
 export function FooterSocial() {
   return (
-    <div className={classes.footer}>
-      <Container className={classes.inner}>
-        <Image
-          src={`${process.env.NEXT_PUBLIC_BASE_PATH}/HopeGives-full-color.png`}
-          alt="footer-logo"
-          width={70}
-          height={50}
-        />
-        <Box>
-          Powered by{" "}
-          <Link href={"https://www.colchuckconsulting.com"} target={"_blank"}>
-            Colchuck Consulting
-          </Link>
+    <Box className={classes.footerBorder}>
+      <Container className={classes.wrapper}>
+        <Box className={classes.footerFlexItem}>
+          <Image
+            src={`${process.env.NEXT_PUBLIC_BASE_PATH}/HopeGives-full-color.png`}
+            alt="footer-logo"
+            width={70}
+            height={100}
+            className={classes.footerLogo}
+          />
         </Box>
-        <Group
-          gap={0}
-          className={classes.links}
-          justify="flex-end"
-          wrap="nowrap"
-        >
+        <Box className={classes.footerFlexItem}>
+          <Title ta="center" order={5} className={classes.title}>
+            Quick Links
+          </Title>
+          <Text ta="center">Search Stories</Text>
+          <Text ta="center">Create a Story</Text>
+          <Text ta="center">How it Works</Text>
+          <Text ta="center">Our Mission</Text>
+        </Box>
+        <Box className={classes.footerFlexItem}>
+          <Title ta="center" order={5} className={classes.title}>
+            Resources{" "}
+          </Title>
+          <Text ta="center">Help</Text>
+          <Text ta="center">FAQ</Text>
+          <Text ta="center">Pricing</Text>
+          <Text ta="center">Contact Us</Text>
+        </Box>
+        <Box className={classes.footerFlexItem} ta="center">
           <ActionIcon size="lg" color="gray" variant="subtle">
             <IconBrandTwitter
               style={{ width: rem(18), height: rem(18) }}
@@ -49,8 +67,8 @@ export function FooterSocial() {
               stroke={1.5}
             />
           </ActionIcon>
-        </Group>
+        </Box>
       </Container>
-    </div>
+    </Box>
   );
 }
