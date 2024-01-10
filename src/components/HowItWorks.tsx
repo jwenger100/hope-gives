e47c4f218495fc87f70059ca3d36b86932f57f12
kylejoe <halterman.kyle@gmail.com>
2024-01-09 22:10:16 -0800

    Changes to container height

--- src/components/HowItWorks.tsx
+++ src/components/HowItWorks.tsx
@@ -23,7 +23,7 @@ const HowItWorksComponent = () => {
         boxShadow: "var(--mantine-shadow-lg)",
         borderRadius: "20px",
         border: "1.25px solid #dee2e6",
-        minHeight: "100vh",
+        minHeight: "100%",
       }}
       pb={"md"}
       mb={"xl"}
